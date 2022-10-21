e90ed3de4308591930e65239b3a4de9a60330bc3
JubacAwal <jubacawal26@gmail.com>
2022-10-21 22:11:26 +0800

    Prototype

--- app/src/main/java/cc17/project/pharmapill/LoginActivity.java
+++ app/src/main/java/cc17/project/pharmapill/LoginActivity.java
@@ -5,20 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         TextView btn=findViewById(R.id.textSignUp);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                openDashboardActivity();
+            }
+        });
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+
             }
         });
     }
+    public void openDashboardActivity()
+    {
+        Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+        startActivity(intent);
+    }
+
 }
